db3fbb569efd6a37ecce99af17f42af1fcaae3c9
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-12-24 19:13:38 +0100

    added list view

--- project-pwa/src/app/pages/components-list/components-list.component.ts
+++ project-pwa/src/app/pages/components-list/components-list.component.ts
@@ -3,11 +3,15 @@ import { RouterLink } from '@angular/router';
 import { ComponentsService } from '../../services/components.service';
 import { Product } from '../../models/component.interface';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-components-list',
   standalone: true,
-  imports: [RouterLink, MatProgressSpinnerModule],
+  imports: [RouterLink, MatProgressSpinnerModule, MatTableModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './components-list.component.html',
   styleUrl: './components-list.component.scss'
 })
@@ -15,6 +19,10 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 export class ComponentsListComponent {
   products = signal<Product[]>([]);
   showSpinner = false;
+  cardsView = false;
+  tableView = true;
+  columnsToDisplay: string[] = ['id', 'title'];
+  dataSource = this.products;
 
   constructor(private componentsService: ComponentsService) {
 
@@ -25,7 +33,19 @@ export class ComponentsListComponent {
     this.componentsService.getProducts().subscribe((res) => {
       this.showSpinner = false;
       this.products.set(res.slice(0, 20));
+      this.dataSource = this.products;
     });
-    
+  }
+
+  changetoTable(): void {
+    this.cardsView = false;
+    this.tableView = true;
+    console.log(this.cardsView, this.tableView);
+  }
+
+  changetoCards(): void {
+    this.cardsView = true;
+    this.tableView = false;
+    console.log(this.cardsView, this.tableView);
   }
 }
